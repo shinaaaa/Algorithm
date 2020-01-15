5161205e9b930effff5701fca9783ff188b4cab1
shin_kim <37543606+shinaaaa@users.noreply.github.com>
2020-01-15 15:11:56 +0900

    bubbleSort

--- Java/ExBubbleSort.java
+++ Java/ExBubbleSort.java
@@ -1,27 +1,31 @@
 package Java;
 
-import java.util.ArrayList;
 import java.util.Random;
-import java.util.Scanner;
-
-/**
- * ExBubbleSort
- */
+import java.util.ArrayList;
 
 public class ExBubbleSort {
   public static void main(String[] args) {
-    Random rand = new Random();
-    ArrayList<Integer> arrayList = new ArrayList<Integer>();
-    Scanner scan = new Scanner(System.in);
 
-    int range = scan.nextInt();
-    scan.close();
+    ArrayList<Integer> arrayList = new ArrayList<Integer>();
+    Random rand = new Random();
+
+    int range = 10;
 
     for (int i = 0; i < range; i++) {
       arrayList.add(rand.nextInt(range));
     }
-    System.out.print(arrayList);
-    for (int i = arrayList.size() - 1; i >= 0; i--) {
+    System.out.println(arrayList);
+
+    for (int i = 0; i < arrayList.size(); i++) {
+      for (int j = i + 1; j < arrayList.size(); j++) {
+        if (arrayList.get(i) > arrayList.get(j)) {
+          int num = arrayList.get(i);
+          arrayList.set(i, arrayList.get(j));
+          arrayList.set(j, num);
+          System.out.println(arrayList);
+        }
+      }
     }
+    System.out.println(arrayList);
   }
 }
