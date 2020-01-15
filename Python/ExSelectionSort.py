13f1b2ec6955f3219ae70edca3730dd9a87c5392
shin_kim <37543606+shinaaaa@users.noreply.github.com>
2020-01-15 15:10:40 +0900

    selectionSort

---
+++ Python/ExSelectionSort.py
@@ -0,0 +1,18 @@
+import random
+
+
+def ExSelectionSort(data):
+    for stand in range(len(data) - 1):
+        lowest = stand
+        for index in range(stand + 1, len(data)):
+            if data[lowest] > data[index]:
+                lowest = index
+        data[lowest], data[stand] = data[stand], data[lowest]
+    return data
+
+
+data_list = random.sample(range(100), 10)
+print(data_list)
+
+selectionSort = ExSelectionSort(data_list)
+print(selectionSort)
